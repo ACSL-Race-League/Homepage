00f96904a91a40941308798e63e950268ea3a664
SeppeVuylsteke <seppe.vuylsteke@student.vives.be>
2025-12-18 19:25:02 +0100

    Homepage

---
+++ Javascript/script.js
@@ -0,0 +1,5 @@
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.offscreen');
+
+hamMenu.addEventListener('click', () => {hamMenu.classList.toggle('active');offScreenMenu.classList.toggle('active');});
